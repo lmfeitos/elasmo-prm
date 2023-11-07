971e509046bb59c7269245f182c83fec930fd788
Allie Caughman <acaughman@ucsb.edu>
2023-11-07 10:24:11 -0800

    load data

--- src/randomforest_fig.R
+++ src/randomforest_fig.R
@@ -1,2 +1,6 @@
 library(tidyverse)
-library(patchwork)
+
+predictions = read_csv(here::here("data", "full_model_predictions.csv"))
+trawl = read_csv(here::here("data", "trawl_model_predictions.csv")) 
+gillnet = read_csv(here::here("data", "gillnet_model_predictions.csv")) 
+
