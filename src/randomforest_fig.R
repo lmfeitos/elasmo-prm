d928c911ea3d8feba432d6cb4b56e0e6e267f0e9
Allie Caughman <acaughman@ucsb.edu>
2023-11-20 15:55:33 -0800

    remove old html

--- src/randomforest_fig.R
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-
-predictions = read_csv(here::here("data", "full_model_predictions.csv"))
-trawl = read_csv(here::here("data", "trawl_model_predictions.csv")) 
-gillnet = read_csv(here::here("data", "gillnet_model_predictions.csv")) 
-
