464327e341969c27e1bb9b5dcdef2c6417c99c69
Allie Caughman <acaughman@ucsb.edu>
2023-11-21 17:23:49 -0800

    update for error

--- src/mortality_clustering.R
+++ src/mortality_clustering.R
@@ -2,14 +2,15 @@ library(tidyverse)
 library(FactoMineR)
 library(factoextra)
 library(ggfortify)
+library(patchwork)
 
 # Sim Results -------------------------------------------------------------
 
-r_data = sim_data %>%
+sim_data <- read_csv(here::here("data", "simulation_data.csv")) %>%
   select(r_value, scientific_name)
 
 sim_results <- read_csv(here::here("data", "simulation_results.csv")) %>% 
-  left_join(r_data)
+  left_join(sim_data)
 
 sim_sub = sim_results %>% 
   filter(t == 200) %>% 
@@ -38,14 +39,16 @@ ggplot(sim_sub %>% filter(mort_scenario != "BAU")) +
 
 sim_pca_w_species = sim_sub %>%  
   pivot_wider(names_from = mort_scenario, values_from = c(avm, prm, total_mort, n_div_k)) %>% 
-  drop_na() %>% 
   ungroup()
 
 sim_pca = sim_pca_w_species %>% 
-  select(-scientific_name)
+  select(-scientific_name)%>% 
+  mutate(
+    across(everything(), ~replace_na(.x, 0))
+  )
 
 fviz_nbclust(sim_pca, FUNcluster = kmeans) 
-km.out <- kmeans(sim_pca, centers = 3, nstart = 20)
+km.out <- kmeans(sim_pca, centers = 4, nstart = 20)
 
 sim_pca_w_species$kmeans = as.factor(km.out$cluster)
 
@@ -55,13 +58,33 @@ sim_pca_w_species = sim_pca_w_species %>%
 sim_sub = sim_sub %>% 
   full_join(sim_pca_w_species)
 
-ggplot(sim_sub) +
-  geom_point(aes(n_div_k, r_value, color = kmeans)) 
+p1 = ggplot(sim_sub) +
+  geom_point(aes(n_div_k, r_value, color = kmeans)) +
+  theme_bw() +
+  labs(x = "N/K",
+       y = "Growth Rate",
+       color = "Group") +
+  scale_color_viridis_d()
 
-ggplot(sim_sub) +
-  geom_point(aes(n_div_k, avm, color = kmeans))
+p2 = ggplot(sim_sub) +
+  geom_point(aes(n_div_k, avm, color = kmeans)) +
+  theme_bw() +
+  labs(x = "N/K",
+       y = "At Vessel Mortality",
+       color = "Group") +
+  scale_color_viridis_d()
 
-ggplot(sim_sub) +
-  geom_point(aes(n_div_k, prm, color = kmeans)) 
+p3 = ggplot(sim_sub) +
+  geom_point(aes(n_div_k, prm, color = kmeans))  +
+  theme_bw() +
+  labs(x = "N/K",
+       y = "Post Release Mortality",
+       color = "Group") +
+  scale_color_viridis_d()
+
+plot1 = p1 /  p2 / p3 + plot_annotation(tag_levels = "A") + plot_layout(guides = "collect")
+plot1
+
+ggsave(plot1, file = paste0("clustering.pdf"), path = here::here("figs"), height = 15, width = 12)
 
 write_csv(sim_sub, here::here("data", "simulation_results_clustered.csv"))
--- src/simulation_vis.R
+++ src/simulation_vis.R
@@ -29,7 +29,7 @@ p <- ggplot() +
              labeller = label_wrap_gen(30), scales = "free") +
   theme_bw() +
   scale_color_viridis_d() +
-  scale_fill_manual(values = c("#d0e11c","#a0da39", "#73d056")) +
+  scale_fill_manual(values = c("#d0e11c","#a0da39", "#73d056", "#4ac16d")) +
   labs(
     x = "Time",
     y = "N/K",
@@ -45,7 +45,7 @@ p <- ggplot() +
   coord_cartesian(clip="off", ylim = c(0, 1))
 p
 
-ggsave(p, file = paste0("initial_sim_0_2_post.pdf"), path = here::here("figs"), height = 20, width = 25)
+ggsave(p, file = paste0("initial_sim_0_2.pdf"), path = here::here("figs"), height = 20, width = 25)
 
 sim_sub = sim_results %>% 
   filter(t == 10 | t == 200) %>% 
@@ -53,20 +53,22 @@ sim_sub = sim_results %>%
   distinct()
 
 no_cg_sub = sim_sub %>% 
-  filter(scenario != "CQ")
+  filter(scenario != "CQ")%>% 
+  right_join(clustered_post)
 
 errors_mort = no_cg_sub %>% 
-  select(t, scenario, avm, prm, scientific_name, mort_scenario, pop.array, n_div_k, total_mort) %>% 
-  distinct() %>% 
   ungroup() %>% 
+  select(t, scenario, avm, prm, scientific_name, mort_scenario, pop.array, n_div_k, total_mort, kmeans) %>% 
+  distinct() %>% 
   group_by(scientific_name, mort_scenario) %>% 
   filter(total_mort == min(total_mort) | total_mort == max(total_mort)) %>% 
   filter(mort_scenario %in% c("BAU", "Low Mortality", "High Mortality")) %>% 
   distinct() %>% 
-  ungroup()
+  ungroup() %>%
+  mutate(scientific_name = as.factor(scientific_name))
 
 p2 = ggplot(errors_mort)  +
-  geom_rect(data = facet_df, aes(xmin = -Inf, xmax = Inf, ymin = 1.1, ymax = 1.45, fill = as.factor(kmeans)), alpha = 0.4) +
+  geom_rect(data = clustered_post, aes(xmin = -Inf, xmax = Inf, ymin = 1.1, ymax = 1.45, fill = as.factor(kmeans)), alpha = 0.4) +
   geom_line(aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
   geom_point(aes(t, n_div_k, color = mort_scenario)) +
   geom_hline(yintercept = 0.5,
@@ -74,11 +76,11 @@ p2 = ggplot(errors_mort)  +
              alpha = 0.5,
              linetype = "dashed") +
   scale_y_continuous(breaks = c(0, 0.5, 1)) +
-  facet_wrap(~scientific_name,
+  facet_wrap(~fct_reorder(as.factor(scientific_name), kmeans),
              labeller = label_wrap_gen(30), scales = "free") +
   theme_bw() +
   scale_color_viridis_d() +
-  scale_fill_manual(values = c("#d0e11c","#a0da39", "#73d056")) +
+  scale_fill_manual(values = c("#d0e11c","#a0da39", "#73d056", "#4ac16d")) +
   labs(
     x = "Time",
     y = "N/K",
@@ -96,45 +98,53 @@ p2
 
 ggsave(p2, file = paste0("timepoints_0_2.pdf"), path = here::here("figs"), height = 20, width = 25)
 
-
-
-
 # Species Sub Plots -------------------------------------------------------
-species_sub = c("Lamna nasus", "Alopias vulpinus", "Carcharhinus galapagensis", "Carcharhinus limbatus", "Galeocerdo cuvier", "Isurus oxyrinchus", "Rhincodon typus", "Sphyrna mokarran", "Squalus acanthias")
 
-no_cq = no_cq %>% 
+species_sub = c("Raja montagui", "Prionace glauca", "Carcharhinus limbatus", "Galeorhinus galeus", "Galeocerdo cuvier", "Rhincodon typus", "Squalus acanthias", "Raja undulata", "Mustelus lunulatus")
+
+no_cq_sub = no_cq %>% 
   filter(scientific_name %in% species_sub)
 
-p <- ggplot(no_cq, aes(t, n_div_k)) +
-  geom_line(aes(color = scenario, group = total_mort)) +
+clustered_post_sub = clustered_post %>% 
+  filter(scientific_name %in% species_sub)
+
+p <- ggplot() +
+  geom_rect(data = clustered_post_sub, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.1, fill = as.factor(kmeans)), alpha = 0.4) +
+  geom_line(data = no_cq_sub, aes(t, n_div_k, color = scenario, group = total_mort)) +
   geom_hline(yintercept = 0.5,
              color = "gray",
              alpha = 0.5,
              linetype = "dashed") +
   scale_y_continuous(breaks = c(0, 0.5, 1)) +
-  facet_wrap(~scientific_name,
-             labeller = label_wrap_gen(30)) +
+  facet_wrap(~fct_reorder(as.factor(scientific_name), kmeans),
+             labeller = label_wrap_gen(30), scales = "free") +
   theme_bw() +
   scale_color_viridis_d() +
+  scale_fill_manual(values = c("#d0e11c","#a0da39", "#73d056", "#4ac16d")) +
   labs(
     x = "Time",
     y = "N/K",
-    color = "Scenario"
+    color = "Scenario",
+    fill = "Group"
   ) +
   theme(panel.grid.minor = element_blank(),
         panel.grid.major.x = element_blank(),
-        strip.background = element_rect(fill = "transparent"),
+        strip.background = element_rect(fill=NA),
         strip.text.x = element_text(size = 8, color = "black", face = "bold.italic"),
         axis.title = element_text(size = 10, color = "black"),
-        axis.text = element_text(size = 10, color = "black"))
+        axis.text = element_text(size = 10, color = "black")) +
+  coord_cartesian(clip="off", ylim = c(0, 1))
 p
 
 ggsave(p, file = paste0("species_full_0_2.pdf"), path = here::here("figs"), height = 15, width = 20)
 
-errors_mort = errors_mort %>% 
-  filter(scientific_name %in% species_sub)
+errors_mort_sub = errors_mort %>% 
+  filter(scientific_name %in% species_sub)%>%
+  mutate(scientific_name = as.factor(scientific_name)) %>% 
+  mutate(scientific_name = fct_reorder(scientific_name, kmeans))
 
-p2 = ggplot(errors_mort) +
+p2 = ggplot(errors_mort_sub)+
+  geom_rect(data = clustered_post_sub, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.2, fill = as.factor(kmeans)), alpha = 0.4) +
   geom_line(aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
   geom_point(aes(t, n_div_k, color = mort_scenario)) +
   geom_hline(yintercept = 0.5,
@@ -142,22 +152,24 @@ p2 = ggplot(errors_mort) +
              alpha = 0.5,
              linetype = "dashed") +
   scale_y_continuous(breaks = c(0, 0.5, 1)) +
-  facet_wrap(~scientific_name,
-             labeller = label_wrap_gen(30)) +
-  facet_wrap(~scientific_name) +
+  facet_wrap(~fct_reorder(as.factor(scientific_name), kmeans),
+             labeller = label_wrap_gen(30), scales = "free") +
   theme_bw() +
+  scale_color_viridis_d() +
+  scale_fill_manual(values = c("#d0e11c","#a0da39", "#73d056", "#4ac16d")) +
   labs(
     x = "Time",
     y = "N/K",
-    color = "Scenario"
-  ) + 
-  scale_color_viridis_d()+
+    color = "Scenario",
+    fill = "Group"
+  ) +
   theme(panel.grid.minor = element_blank(),
         panel.grid.major.x = element_blank(),
-        strip.background = element_rect(fill = "transparent"),
+        strip.background = element_rect(fill=NA),
         strip.text.x = element_text(size = 8, color = "black", face = "bold.italic"),
         axis.title = element_text(size = 10, color = "black"),
-        axis.text = element_text(size = 10, color = "black"))
+        axis.text = element_text(size = 10, color = "black")) +
+  coord_cartesian(clip="off", ylim = c(0, 1))
 p2
 
 ggsave(p2, file = paste0("species_timepoints_0_2.pdf"), path = here::here("figs"), height = 15, width = 20)
