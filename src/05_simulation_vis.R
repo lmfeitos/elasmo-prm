52fa48339ffb7810354874a430676f20b45fea9b
lmfeitos <lmfeitosa@ucsb.edu>
2024-01-15 19:26:55 -0800

    updated supplemental plots.

--- src/05_simulation_vis.R
+++ src/05_simulation_vis.R
@@ -49,7 +49,7 @@ no_cq = sim_results %>%
     TRUE ~ redlist_category
   ))  %>% 
   mutate(redlist_category = fct_relevel(as.factor(redlist_category), c("CR", "EN", "VU", "NT", "LC", "DD", "NE"))) 
-  arrange(redlist_category, scientific_name)
+  #arrange(redlist_category, scientific_name)
 
 
 no_cq_sci <- no_cq %>% 
@@ -58,7 +58,7 @@ no_cq_sci <- no_cq %>%
 
 p <- 
   ggplot() +
-  geom_rect(data = no_cq, aes(xmin = -Inf, xmax = Inf, ymin = 1.1, ymax = 1.9, fill = as.factor(redlist_category))) +
+  geom_rect(data = no_cq, aes(xmin = -Inf, xmax = Inf, ymin = 1.1, ymax = 1.7, fill = as.factor(redlist_category))) +
   geom_line(data = no_cq, aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
   geom_hline(yintercept = 0.5,
              color = "gray",
@@ -70,7 +70,7 @@ p <-
              labeller = label_wrap_gen(15)) +
   theme_bw() +
   scale_color_viridis_d() +
-  scale_fill_manual(values = c("#bd0026", "#f03b20", "#fd8d3c", "#fecc5c", "#ffffb2", "grey", "white")) +
+  scale_fill_manual(values = c("#E31A1C", "#FD8D3C", "#FED976", "#91CF60", "#1A9850", "grey", "white")) +
   labs(
     x = "Time",
     y = "N/K",
@@ -90,29 +90,41 @@ ggsave(p, file = paste0("initial_sim.pdf"), path = here::here("figs", "supp"), h
 
 # Species Sub Plots -------------------------------------------------------
 
-species_sub = c("Prionace glauca", "Carcharhinus limbatus", "Isurus oxyrinchus", 
-                "Mustelus canis", "Squalus acanthias", "Alopias vulpinus", 
+species_sub = c("Prionace glauca", "Carcharhinus limbatus", "Isurus oxyrinchus", "Squalus acanthias", "Alopias vulpinus", 
                 "Pseudocarcharias kamoharai", "Carcharhinus falciformis", "Sphyrna mokarran",
-                "Carcharhinus hemiodon", "Squatina squatina", "Isogomphodon oxyrhynchus", "Squalus blainville",
-                "Echinorhinus cookei", "Sphyrna corona", "Galeorhinus galeus")
+                "Carcharhinus hemiodon", "Squatina squatina", "Sphyrna corona", "Galeorhinus galeus")
 
 no_cq_sub = no_cq %>% 
   filter(scientific_name %in% species_sub) 
+
+tag_text <- data.frame(t = c(180, 180, 180),
+                       n_div_k = c(0.98, 0.98, 0.98),
+                       label = c("A", "B", "C"),
+                       scientific_name = factor(c("Prionace glauca", "Pseudocarcharias kamoharai", "Alopias vulpinus", "Carcharhinus falciformis",
+                                  "Isurus oxyrinchus", "Squalus acanthias", "Carcharhinus hemiodon", "Sphyrna corona",
+                                  "Squatina squatina", "Sphyrna mokarran", "Carcharhinus limbatus", "Galeorhinus galeus")))
 
 p <- ggplot() +
   geom_hline(yintercept = 0.5,
              color = "gray",
              linetype = "dashed") +
   geom_rect(data = no_cq_sub,
-            aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.13, fill = as.factor(redlist_category))) +
-  geom_line(data = no_cq_sub, aes(t, n_div_k, color = mort_scenario, group = total_mort), linewidth = 2) +
+            aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.145, fill = as.factor(redlist_category))) +
+  geom_line(data = no_cq_sub, aes(t, n_div_k, color = mort_scenario, group = total_mort), linewidth = 1.5) +
+  geom_text(data = tag_text %>% 
+              filter(scientific_name %in% c("Prionace glauca", "Isurus oxyrinchus", "Squatina squatina")),
+            aes(x = t, y = n_div_k, label = label),
+            color = "black",
+            fontface = "bold",
+            size = 5) +
   scale_y_continuous(breaks = c(0, 0.5, 1)) +
-  facet_wrap(~scientific_name,
-             #labeller = label_wrap_gen(10),
-             nrow = 4) +
+  facet_wrap( ~ factor(scientific_name,
+                      levels = c("Prionace glauca", "Pseudocarcharias kamoharai", "Alopias vulpinus", "Carcharhinus falciformis",
+                                 "Isurus oxyrinchus", "Squalus acanthias", "Carcharhinus hemiodon", "Sphyrna corona",
+                                  "Squatina squatina", "Sphyrna mokarran", "Carcharhinus limbatus", "Galeorhinus galeus"))) +
   theme_bw() +
   scale_color_viridis_d() +
-  scale_fill_manual(values = c("#E31A1C", "#FD8D3C", "#FED976", "#91CF60", "#1A9850", "gray")) +
+  scale_fill_manual(values = c("#E31A1C", "#FD8D3C", "#FED976", "#91CF60", "#1A9850")) +
   labs(
     x = "Time",
     y = "N/K",
@@ -128,13 +140,13 @@ p <- ggplot() +
   coord_cartesian(clip="off", ylim = c(0, 1))
 p
 
-ggsave(p, file = paste0("species_full.pdf"), path = here::here("figs"), height = 15, width = 20)
+ggsave(p, file = paste0("species_full.pdf"), path = here::here("figs"), height = 10, width = 10)
 
 
 percent_calc = sim_results %>% 
   mutate(f_mort = ((100 * f) - (100 * f * (1-mid_avm) * (1-mid_prm)))/100) %>% 
   select(scientific_name, f, f_mort, mid_avm, mid_prm) %>% 
-  distinct()%>% 
+  distinct() %>% 
   mutate(percent_diff = (f - f_mort) / f * 100) %>% 
   mutate(mean_diff = mean(percent_diff, na.rm=TRUE))
 
