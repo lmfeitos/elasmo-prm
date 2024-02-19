67881f8e68a4da0a30cb1bf3e46a39dff727b785
Allie Caughman <acaughman@ucsb.edu>
2024-02-19 14:40:11 -0800

    update plot

--- src/05_simulation_vis.R
+++ src/05_simulation_vis.R
@@ -59,10 +59,22 @@ no_cq_sci <- no_cq %>%
   distinct(scientific_name) %>% 
   pull()
 
-p <- 
-  ggplot() +
-  geom_rect(data = no_cq, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.7, fill = as.factor(redlist_category))) +
-  geom_line(data = no_cq, aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
+no_cq_cr = no_cq %>% 
+  filter(redlist_category == "CR")
+no_cq_en = no_cq %>% 
+  filter(redlist_category == "EN")
+no_cq_vu = no_cq %>% 
+  filter(redlist_category == "VU")
+no_cq_nt = no_cq %>% 
+  filter(redlist_category == "NT")
+no_cq_lc = no_cq %>% 
+  filter(redlist_category == "LC")
+no_cq_dd = no_cq %>% 
+  filter(redlist_category == "DD")
+
+p1 = ggplot() +
+  geom_rect(data = no_cq_cr, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.5, fill = as.factor(redlist_category))) +
+  geom_line(data = no_cq_cr, aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
   geom_hline(yintercept = 0.5,
              color = "gray",
              alpha = 0.5,
@@ -87,9 +99,190 @@ p <-
         axis.title = element_text(color = "black"),
         axis.text = element_text( color = "black")) +
   coord_cartesian(clip="off", ylim = c(0, 1))
-p
 
-ggsave(p, file = paste0("initial_sim.pdf"), path = here::here("figs", "supp"), height = 30, width = 30)
+ggsave(p1, file = paste0("initial_sim_cr.pdf"), path = here::here("figs", "supp"), height = 12, width = 12)
+
+p2 = ggplot() +
+  geom_rect(data = no_cq_en, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.85, fill = as.factor(redlist_category))) +
+  geom_line(data = no_cq_en, aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
+  geom_hline(yintercept = 0.5,
+             color = "gray",
+             alpha = 0.5,
+             linetype = "dashed") +
+  scale_y_continuous(breaks = c(0, 0.5, 1)) +
+  facet_wrap(~ factor(scientific_name,
+                      levels = no_cq_sci),
+             labeller = label_wrap_gen(15)) +
+  theme_bw(base_size = 14) +
+  scale_color_viridis_d() +
+  scale_fill_manual(values = c("#FD8D3C", "#FED976", "#91CF60", "#1A9850", "grey", "white", "#E31A1C")) +
+  labs(
+    x = "Time",
+    y = "N/K",
+    color = "Scenario",
+    fill = "IUCN Category"
+  ) +
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        strip.background = element_rect(fill=NA),
+        strip.text.x = element_text(color = "black", face = "bold.italic"),
+        axis.title = element_text(color = "black"),
+        axis.text = element_text( color = "black")) +
+  coord_cartesian(clip="off", ylim = c(0, 1))
+
+ggsave(p2, file = paste0("initial_sim_en.pdf"), path = here::here("figs", "supp"), height = 12, width = 12)
+
+p3 = ggplot() +
+  geom_rect(data = no_cq_vu, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.85, fill = as.factor(redlist_category))) +
+  geom_line(data = no_cq_vu, aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
+  geom_hline(yintercept = 0.5,
+             color = "gray",
+             alpha = 0.5,
+             linetype = "dashed") +
+  scale_y_continuous(breaks = c(0, 0.5, 1)) +
+  facet_wrap(~ factor(scientific_name,
+                      levels = no_cq_sci),
+             labeller = label_wrap_gen(15)) +
+  theme_bw(base_size = 14) +
+  scale_color_viridis_d() +
+  scale_fill_manual(values = c("#FED976", "#91CF60", "#1A9850", "grey", "white", "#E31A1C", "#FD8D3C" )) +
+  labs(
+    x = "Time",
+    y = "N/K",
+    color = "Scenario",
+    fill = "IUCN Category"
+  ) +
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        strip.background = element_rect(fill=NA),
+        strip.text.x = element_text(color = "black", face = "bold.italic"),
+        axis.title = element_text(color = "black"),
+        axis.text = element_text( color = "black")) +
+  coord_cartesian(clip="off", ylim = c(0, 1))
+
+ggsave(p3, file = paste0("initial_sim_vu.pdf"), path = here::here("figs", "supp"), height = 12, width = 12)
+
+p4 = ggplot() +
+  geom_rect(data = no_cq_nt, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.65, fill = as.factor(redlist_category))) +
+  geom_line(data = no_cq_nt, aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
+  geom_hline(yintercept = 0.5,
+             color = "gray",
+             alpha = 0.5,
+             linetype = "dashed") +
+  scale_y_continuous(breaks = c(0, 0.5, 1)) +
+  facet_wrap(~ factor(scientific_name,
+                      levels = no_cq_sci),
+             labeller = label_wrap_gen(15)) +
+  theme_bw(base_size = 14) +
+  scale_color_viridis_d() +
+  scale_fill_manual(values = c( "#91CF60", "#1A9850", "grey", "white", "#E31A1C", "#FD8D3C", "#FED976" )) +
+  labs(
+    x = "Time",
+    y = "N/K",
+    color = "Scenario",
+    fill = "IUCN Category"
+  ) +
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        strip.background = element_rect(fill=NA),
+        strip.text.x = element_text(color = "black", face = "bold.italic"),
+        axis.title = element_text(color = "black"),
+        axis.text = element_text( color = "black")) +
+  coord_cartesian(clip="off", ylim = c(0, 1))
+
+ggsave(p4, file = paste0("initial_sim_nt.pdf"), path = here::here("figs", "supp"), height = 12, width = 12)
+
+p5 = ggplot() +
+  geom_rect(data = no_cq_lc, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 2, fill = as.factor(redlist_category))) +
+  geom_line(data = no_cq_lc, aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
+  geom_hline(yintercept = 0.5,
+             color = "gray",
+             alpha = 0.5,
+             linetype = "dashed") +
+  scale_y_continuous(breaks = c(0, 0.5, 1)) +
+  facet_wrap(~ factor(scientific_name,
+                      levels = no_cq_sci),
+             labeller = label_wrap_gen(15)) +
+  theme_bw(base_size = 14) +
+  scale_color_viridis_d() +
+  scale_fill_manual(values = c( "#1A9850", "grey", "white", "#E31A1C", "#FD8D3C", "#FED976" )) +
+  labs(
+    x = "Time",
+    y = "N/K",
+    color = "Scenario",
+    fill = "IUCN Category"
+  ) +
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        strip.background = element_rect(fill=NA),
+        strip.text.x = element_text(color = "black", face = "bold.italic"),
+        axis.title = element_text(color = "black"),
+        axis.text = element_text( color = "black")) +
+  coord_cartesian(clip="off", ylim = c(0, 1))
+
+ggsave(p5, file = paste0("initial_sim_lc.pdf"), path = here::here("figs", "supp"), height = 15, width = 18)
+
+p6 = ggplot() +
+  geom_rect(data = no_cq_dd, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.38, fill = as.factor(redlist_category))) +
+  geom_line(data = no_cq_dd, aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
+  geom_hline(yintercept = 0.5,
+             color = "gray",
+             alpha = 0.5,
+             linetype = "dashed") +
+  scale_y_continuous(breaks = c(0, 0.5, 1)) +
+  facet_wrap(~ factor(scientific_name,
+                      levels = no_cq_sci),
+             labeller = label_wrap_gen(15)) +
+  theme_bw(base_size = 14) +
+  scale_color_viridis_d() +
+  scale_fill_manual(values = c( "grey", "white", "#E31A1C", "#FD8D3C", "#FED976" )) +
+  labs(
+    x = "Time",
+    y = "N/K",
+    color = "Scenario",
+    fill = "IUCN Category"
+  ) +
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        strip.background = element_rect(fill=NA),
+        strip.text.x = element_text(color = "black", face = "bold.italic"),
+        axis.title = element_text(color = "black"),
+        axis.text = element_text( color = "black")) +
+  coord_cartesian(clip="off", ylim = c(0, 1))
+
+ggsave(p6, file = paste0("initial_sim_dd.pdf"), path = here::here("figs", "supp"), height = 12, width = 12)
+
+# p <- 
+#   ggplot() +
+#   geom_rect(data = no_cq, aes(xmin = -Inf, xmax = Inf, ymin = 1.05, ymax = 1.7, fill = as.factor(redlist_category))) +
+#   geom_line(data = no_cq, aes(t, n_div_k, color = mort_scenario, group = total_mort)) +
+#   geom_hline(yintercept = 0.5,
+#              color = "gray",
+#              alpha = 0.5,
+#              linetype = "dashed") +
+#   scale_y_continuous(breaks = c(0, 0.5, 1)) +
+#   facet_wrap(~ factor(scientific_name,
+#                       levels = no_cq_sci),
+#              labeller = label_wrap_gen(15)) +
+#   theme_bw(base_size = 14) +
+#   scale_color_viridis_d() +
+#   scale_fill_manual(values = c("#E31A1C", "#FD8D3C", "#FED976", "#91CF60", "#1A9850", "grey", "white")) +
+#   labs(
+#     x = "Time",
+#     y = "N/K",
+#     color = "Scenario",
+#     fill = "IUCN Category"
+#   ) +
+#   theme(panel.grid.minor = element_blank(),
+#         panel.grid.major.x = element_blank(),
+#         strip.background = element_rect(fill=NA),
+#         strip.text.x = element_text(color = "black", face = "bold.italic"),
+#         axis.title = element_text(color = "black"),
+#         axis.text = element_text( color = "black")) +
+#   coord_cartesian(clip="off", ylim = c(0, 1))
+# p
+# 
+# ggsave(p, file = paste0("initial_sim.pdf"), path = here::here("figs", "supp"), height = 30, width = 30)
 
 # Species Sub Plots -------------------------------------------------------
 
