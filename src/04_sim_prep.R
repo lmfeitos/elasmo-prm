0f6fdca3cb00daa93feddd777ad4e82f741fd11c
Allie Caughman <acaughman@ucsb.edu>
2024-03-12 10:42:08 -0700

    style and comment

--- src/04_sim_prep.R
+++ src/04_sim_prep.R
@@ -1,74 +1,83 @@
 library(tidyverse)
 library(FishLife)
 
-predictions_full = read_csv(here::here("data", "full_model_predictions.csv")) 
+# read in random forest predictions
+predictions_full <- read_csv(here::here("data", "full_model_predictions.csv"))
 
-r_growth = read_csv(here::here("data", "intrinsic_pop_growth_rates_full.csv")) %>% 
-  select(scientific_name, mean_r) %>% 
+# read in r from literature review
+r_growth <- read_csv(here::here("data", "intrinsic_pop_growth_rates_full.csv")) %>%
+  select(scientific_name, mean_r) %>%
   distinct()
 
-fish_data = FishLife::FishBase_and_RAM$beta_gv %>% 
+# get fishlife data
+fish_data <- FishLife::FishBase_and_RAM$beta_gv %>%
   as.data.frame()
 
-data_filled = data.frame()
+# create an empty dataframe to hold r values from fishlife
+data_filled <- data.frame()
 
-predictions = predictions_full %>% 
-  filter(!scientific_name %in% c("Centroscymnus owstoni", "Carcharhinus limbatus/tilstoni", 
-                                 "Bythaelurus bachi", "Bythaelurus naylori",
-                                 "Bythaelurus tenuicephalus", "Carcharhinus obsoletus",
-                                 "Centrophorus lesliei", "Centrophorus longipinnis",
-                                 "Centroselachus crepidater", "Cephaloscyllium formosanum",
-                                 "Chiloscyllium hasselti", "Etmopterus alphus",
-                                 "Etmopterus brosei", "Etmopterus lailae", 
-                                 "Etmopterus marshae", "Etmopterus sheikoi",
-                                 "Hexanchus vitulus", "Mustelus andamanensis",
-                                 "Parmaturus angelae", "Planonasus indicus", 
-                                 "Pliotrema annae", "Pliotrema kajae",
-                                 "Scymnodon ichiharai", "Scymnodon plunketi",
-                                 "Squalus acutipinnis", "Squalus albicaudus",
-                                 "Squalus bahiensis", "Squalus bassi", "Squalus boretzi",
-                                 "Squalus clarkae", "Squalus hawaiiensis", "Squalus lobularis",
-                                 "Squalus mahia", "Squalus margaretsmithae",
-                                 "Squalus quasimodo", "Squatina david",
-                                 "Stegostoma tigrinum", "Acroteriobatus annulatus",
-                                 "Acroteriobatus blochii",  "Acroteriobatus leucospilus",
-                                 "Acroteriobatus ocellatus", "Acroteriobatus omanensis", 
-                                 "Acroteriobatus salalah", "Acroteriobatus variegatus",
-                                 "Glaucostegus cemiculus", "Glaucostegus obtusus", 
-                                 "Glaucostegus thouin", "Pseudobatos buthi",
-                                 "Pseudobatos glaucostigmus","Pseudobatos horkelii",
-                                 "Pseudobatos lentiginosus","Pseudobatos leucorhynchus",
-                                 "Pseudobatos percellens", "Pseudobatos planiceps", 
-                                 "Pseudobatos prahli", "Pseudobatos productus", 
-                                 "Rhinobatos austini", "Rhinobatos borneensis", 
-                                 "Rhinobatos ranongensis"))
+# get r values from fishlife
+predictions <- predictions_full %>%
+  filter(!scientific_name %in% c(
+    "Centroscymnus owstoni", "Carcharhinus limbatus/tilstoni",
+    "Bythaelurus bachi", "Bythaelurus naylori",
+    "Bythaelurus tenuicephalus", "Carcharhinus obsoletus",
+    "Centrophorus lesliei", "Centrophorus longipinnis",
+    "Centroselachus crepidater", "Cephaloscyllium formosanum",
+    "Chiloscyllium hasselti", "Etmopterus alphus",
+    "Etmopterus brosei", "Etmopterus lailae",
+    "Etmopterus marshae", "Etmopterus sheikoi",
+    "Hexanchus vitulus", "Mustelus andamanensis",
+    "Parmaturus angelae", "Planonasus indicus",
+    "Pliotrema annae", "Pliotrema kajae",
+    "Scymnodon ichiharai", "Scymnodon plunketi",
+    "Squalus acutipinnis", "Squalus albicaudus",
+    "Squalus bahiensis", "Squalus bassi", "Squalus boretzi",
+    "Squalus clarkae", "Squalus hawaiiensis", "Squalus lobularis",
+    "Squalus mahia", "Squalus margaretsmithae",
+    "Squalus quasimodo", "Squatina david",
+    "Stegostoma tigrinum", "Acroteriobatus annulatus",
+    "Acroteriobatus blochii", "Acroteriobatus leucospilus",
+    "Acroteriobatus ocellatus", "Acroteriobatus omanensis",
+    "Acroteriobatus salalah", "Acroteriobatus variegatus",
+    "Glaucostegus cemiculus", "Glaucostegus obtusus",
+    "Glaucostegus thouin", "Pseudobatos buthi",
+    "Pseudobatos glaucostigmus", "Pseudobatos horkelii",
+    "Pseudobatos lentiginosus", "Pseudobatos leucorhynchus",
+    "Pseudobatos percellens", "Pseudobatos planiceps",
+    "Pseudobatos prahli", "Pseudobatos productus",
+    "Rhinobatos austini", "Rhinobatos borneensis",
+    "Rhinobatos ranongensis"
+  )) # exclude species not within fishlife
 
-for(i in 1:nrow(predictions)) {
-  species = Match_species(genus_species = predictions$scientific_name[i])
-  
-  data_filled = rbind(fish_data[species[[1]],], data_filled)
+# assign r value for matching species to our preditcions
+for (i in 1:nrow(predictions)) {
+  species <- Match_species(genus_species = predictions$scientific_name[i])
+
+  data_filled <- rbind(fish_data[species[[1]], ], data_filled)
 }
 
-predictions$fish_r = data_filled$r
+predictions$fish_r <- data_filled$r
 
-
-pred_r = left_join(predictions_full, r_growth) %>% 
-  distinct() %>% 
-  left_join(predictions)%>% 
+# combine with our data set and remove NAs
+pred_r <- left_join(predictions_full, r_growth) %>%
+  distinct() %>%
+  left_join(predictions) %>%
   mutate(mid_avm = case_when(
-    !is.na(avm_mort) ~ avm_mort, 
+    !is.na(avm_mort) ~ avm_mort,
     is.na(avm_mort) ~ avm_pred
-  )) %>% 
+  )) %>%
   mutate(mid_prm = case_when(
-    !is.na(prm_mort) ~ prm_mort, 
+    !is.na(prm_mort) ~ prm_mort,
     is.na(prm_mort) ~ prm_pred
-  )) %>% 
+  )) %>%
+  # use literature based r if available and fishlife r elsewhere
   mutate(r_value = case_when(
     mean_r < 0 ~ fish_r,
     !is.na(mean_r) ~ mean_r,
     !is.na(fish_r) ~ fish_r
-  ))  %>% 
-  filter(!is.na(r_value)) %>% 
+  )) %>%
+  filter(!is.na(r_value)) %>%
   mutate(dif_r = mean_r - fish_r)
 
 write_csv(pred_r, here::here("data", "simulation_data.csv"))
--- src/05_policy_sim.R
+++ src/05_policy_sim.R
@@ -9,25 +9,28 @@ set.seed(42)
 # 1 Full Retention "FR"
 # 2 Retention Ban ("RB")
 
+# read in data from sim_prep.R
 sim_data <- read_csv(here::here("data", "simulation_data.csv")) %>%
-  select(prm_75, prm_25, avm_25, avm_75, scientific_name, r_value, mid_avm, mid_prm) %>% 
-  group_by(scientific_name) %>% 
-  mutate(prm_75 = max(prm_75),
-         prm_25 = min(prm_25),
-         mid_prm = mean(mid_prm),
-         avm_75 = max(avm_75),
-         avm_25 = min(avm_25),
-         mid_avm = mean(mid_avm),
-         r_value = mean(r_value)) %>% 
-  distinct() %>% 
-  mutate(msy = r_value / 2 ) %>%
+  select(prm_75, prm_25, avm_25, avm_75, scientific_name, r_value, mid_avm, mid_prm) %>%
+  group_by(scientific_name) %>%
+  mutate(
+    prm_75 = max(prm_75),
+    prm_25 = min(prm_25),
+    mid_prm = mean(mid_prm),
+    avm_75 = max(avm_75),
+    avm_25 = min(avm_25),
+    mid_avm = mean(mid_avm),
+    r_value = mean(r_value)
+  ) %>%
+  distinct() %>%
+  mutate(msy = r_value / 2) %>%
   mutate(f = 1.5 * msy)
-    
+
 
 # Functions ---------------------------------------------------------------
 
+# create the simulation based on equation in paper
 sim <- function(t, N_0, K, r, avs, prs, q, f, quota, scenario) {
-  
   # set possible survival when thrown back to 0 as none are thrown back in FR
   if (scenario == "FR") {
     avs <- 0
@@ -46,13 +49,11 @@ sim <- function(t, N_0, K, r, avs, prs, q, f, quota, scenario) {
     dt <- 1
 
     # population growth - fishing + returned bycatch
-    dN.dt = (r * pop.array[i - 1] * (1 - (pop.array[i - 1] / K))) - (q * f * pop.array[i - 1]) + ((q * f * pop.array[i - 1] - quota) * avs * prs) * dt
+    dN.dt <- (r * pop.array[i - 1] * (1 - (pop.array[i - 1] / K))) - (q * f * pop.array[i - 1]) + ((q * f * pop.array[i - 1] - quota) * avs * prs) * dt
     pop.array[i] <- pop.array[i - 1] + dN.dt
   }
 
-  # ((q * f * pop.array[i - 1]) * (1 - quota)) * avs * prs) # by percent
-  # ((q * f * pop.array[i - 1]) - quota) *  avs * prs) # based on count
-
+  # add to dataframe
   pop_df <- as.data.frame(pop.array) %>%
     mutate(t = 1:t) %>% # add time column
     mutate(scenario = scenario) %>%
@@ -63,6 +64,7 @@ sim <- function(t, N_0, K, r, avs, prs, q, f, quota, scenario) {
 
 # Set Up ------------------------------------------------------------------
 
+# simulation parameters
 t <- 200
 K <- 100
 # r = 0.2 #set for each shark
@@ -70,26 +72,24 @@ N_0 <- 100
 # avs = 0.8 # at vessel survival; higher = better for shark (test quantile range from RF)
 # prs = 0.8 # post release survival; higher = better for shark (test quantile range from RF)
 q <- 1 # catchability, set to 1 for ease of fishing pressure, could vary
-# f <- 0.2 # vary for sensitivity
-# quota <- 0 # in count
-quota.array = c(5) # in count
 
 # shark sim loop ----------------------------------------------------------
 
 ## empty dataframe
 sim_results <- data.frame()
 
+# loop through all species and run the simulation
 for (i in 1:nrow(sim_data)) {
   ## set up mortality grid for shark species
   avms <- c(sim_data$avm_25[i], sim_data$mid_avm[i], sim_data$avm_75[i])
   prms <- c(sim_data$prm_25[i], sim_data$mid_prm[i], sim_data$prm_75[i])
-  morts <- expand.grid(avms, prms) %>% 
+  morts <- expand.grid(avms, prms) %>%
     distinct()
   names(morts) <- c("avm", "prm")
 
   species <- sim_data$scientific_name[i]
   r <- sim_data$r_value[i]
-  
+
   f <- sim_data$f[i]
 
   ## single business as usual scenario
@@ -97,8 +97,8 @@ for (i in 1:nrow(sim_data)) {
     mutate(
       avm = 1,
       prm = 1,
-      scientific_name = species, 
-      quota = 0, 
+      scientific_name = species,
+      quota = 0,
       f = f
     )
 
@@ -114,56 +114,45 @@ for (i in 1:nrow(sim_data)) {
         quota = 0,
         f = f
       )
-    
-    sim_results <- rbind(sim_results, rb)
-    
-    # for (k in 1:length(quota.array)) {
-    #   cq <- sim(t, N_0, K, r, 1 - morts$avm[j], 1 - morts$prm[j], q, f, quota.array[k], "CQ") %>%
-    #     mutate(
-    #       avm = morts$avm[j],
-    #       prm = morts$prm[j],
-    #       scientific_name = species,
-    #       quota = quota.array[k]
-    #     )
-    # 
-    #   sim_results <- rbind(sim_results, cq)
-    # }
 
+    sim_results <- rbind(sim_results, rb)
   }
 }
 
-join_data = sim_data %>% 
+join_data <- sim_data %>%
   select(scientific_name, avm_75, prm_75, avm_25, prm_25, mid_avm, mid_prm)
 
 sim_results <- sim_results %>%
   mutate(total_mort = avm * prm) %>%
   full_join(join_data) %>%
-  mutate(n_div_k = pop.array/K)
+  mutate(n_div_k = pop.array / K)
 
-species_max_mort = sim_results %>% 
-  filter(scenario != "FR") %>% 
-  group_by(scientific_name) %>% 
+# get lowest, median, and highest moratlity values used in simulation
+species_max_mort <- sim_results %>%
+  filter(scenario != "FR") %>%
+  group_by(scientific_name) %>%
   summarize(max_mort = max(total_mort))
-species_min_mort = sim_results %>% 
-  filter(scenario != "FR") %>% 
-  group_by(scientific_name) %>% 
+species_min_mort <- sim_results %>%
+  filter(scenario != "FR") %>%
+  group_by(scientific_name) %>%
   summarize(min_mort = min(total_mort))
-species_median_mort = sim_results %>% 
-  filter(scenario != "FR") %>% 
-  select(scientific_name, avm, mid_avm, prm, mid_prm, total_mort) %>% 
+species_median_mort <- sim_results %>%
+  filter(scenario != "FR") %>%
+  select(scientific_name, avm, mid_avm, prm, mid_prm, total_mort) %>%
   distinct() %>%
-  filter(avm == mid_avm & prm == mid_prm) %>% 
-  group_by(scientific_name) %>% 
+  filter(avm == mid_avm & prm == mid_prm) %>%
+  group_by(scientific_name) %>%
   summarize(med_mort = max(total_mort))
 
-summarized = list(species_max_mort, species_median_mort, species_min_mort, sim_results) %>% 
-  reduce(full_join) %>% 
+# manipulate dataframe to create a single row per species
+summarized <- list(species_max_mort, species_median_mort, species_min_mort, sim_results) %>%
+  reduce(full_join) %>%
   mutate(mort_scenario = case_when(
     scenario == "FR" ~ "Full Retention",
     total_mort == med_mort ~ "Median Mortality",
     total_mort == min_mort ~ "Low Mortality",
     total_mort == max_mort ~ "High Mortality"
-  )) %>% 
+  )) %>%
   select(-med_mort, -min_mort, -max_mort)
 
 write_csv(summarized, here::here("data", "simulation_results.csv"))
